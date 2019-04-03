08a9af2b0522a1237f375da98c43d4ac751b89a5
Layla Mira <laylamira@gmail.com>
2019-04-04 00:05:18 +0800

    refactor done

---
+++ js/index.js
@@ -0,0 +1,245 @@
+
+  function closePrint() {
+  document.body.removeChild(this._container_);
+  }
+  
+  function setPrint() {
+    this.contentWindow._container_ = this;
+    this.contentWindow.onbeforeunload = closePrint;
+    this.contentWindow.onafterprint = closePrint;
+    this.contentWindow.focus(); // Required for IE
+    this.contentWindow.print();
+  }
+  
+  function printPage (sURL) {
+    var oHiddFrame = document.createElement("iframe");
+    oHiddFrame.onload = setPrint;
+    oHiddFrame.style.visibility = "hidden";
+    oHiddFrame.style.position = "fixed";
+    oHiddFrame.style.right = "0";
+    oHiddFrame.style.bottom = "0";
+    oHiddFrame.src = sURL;
+    document.body.appendChild(oHiddFrame);
+  }
+  
+
+// Scroll button for Homepage
+$(function() {
+  $('a[href*=\\#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+var mutebtn = document.getElementById("mutebtn");
+var audio = document.getElementById("audio");
+var icon = document.getElementById("btnicon");
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+// Select navigation button
+const menuButton = document.querySelector(".navigation__btn");
+// Add click event listener to menu button
+menuButton.addEventListener("click", showMenu);
+// Selevt navigation list
+const navigationList = document.querySelector(".navigation__list");
+
+// Toggle Navigation Menu
+function showMenu() {
+  menuButton.classList.toggle("navigation__btn--active");
+  // navigationList.classList.toggle("navigation__list--active");
+  if (menuButton.getAttribute("aria-expanded") === "false") {
+    menuButton.setAttribute("aria-expanded", true);
+  } else {
+    menuButton.setAttribute("aria-expanded", false);
+  }
+}
+
+// Select dropdown button
+const dropBtn = document.querySelectorAll(".navigation__drop-btn");
+console.log(typeof dropBtn);
+
+// Set click event listener on each dropdown button
+dropBtn.forEach(v => {
+  v.addEventListener("click", showDropdown);
+});
+
+// Toggle dropdown items
+function showDropdown(e) {
+  console.log(e.target.classList);
+  e.target.classList.toggle("triangle");
+  if (e.target.classList.contains("more")) {
+    console.log("hello");
+    document
+      .querySelector(".navigation__dropdown--more")
+      .classList.toggle("navigation__dropdown--active");
+  } else if (e.target.classList.contains("shop")) {
+    document
+      .querySelector(".navigation__dropdown--shop")
+      .classList.toggle("navigation__dropdown--active");
+  }
+}
+
+
+// Select all links in navigation menu
+const navigationLink = document.querySelectorAll(".navigation__link");
+
+// Set click event listener on each navigation link
+navigationLink.forEach(v => {
+  v.addEventListener("click", closeMenu);
+});
+
+// If a link in the navigation menu is clicked close the menu and change
+// menu button shape
+function closeMenu() {
+  navigationList.classList.remove("navigation__list--active");
+  menuButton.classList.toggle("navigation__btn--active");
+  menuButton.setAttribute("aria-expanded", false);
+}
+
+
+const textButton = document.getElementById("Textbutton");
+if ( textButton ) {
+  textButton.addEventListener('click', function() {
+    if(textButton === undefined) {
+      textButton.unbind('click')
+      console.log('undefined')
+    } else {
+      console.log('hovering')
+    }
+  })
+}
+
+
+// RESPONSIVE FIXED SIDE MENU 
+			$(document).ready(function () {
+			  $('.menuIcon').click(function () {
+				  if ($('.NavigationSlideOut').css("left") == "-300px") {
+					  $('.NavigationSlideOut').animate({left: '0px'}, 350);
+				  } 
+				  else  {
+					  $('.NavigationSlideOut').animate({left: '-300px'}, 350); 
+				  } 
+			  });
+			  $(document).click(function(){
+				if($('.NavigationSlideOut').css('left') == '0px' && $('.menuIcon').hasClass('on') ) {
+				  $('.NavigationSlideOut').animate({left: '-300px'}, 350);
+				  $('.menuIcon').toggleClass("on");
+				}
+			  })
+			  $('.menuIcon').click(function () {
+				$(this).toggleClass("on"); 
+			  }); // animation = -> x
+          });
+		
+
+
+// ENLARGE TEXT BOIIII START --------------------------------------------------------------------
+
+	// Increase/descrease font size
+	$('#increasetext').click(function() {
+		curSize = parseInt($('#content').css('font-size')) + 2;
+		if (curSize <= 32)
+			$('#content').css('font-size', curSize);
+	});
+
+	$('#resettext').click(function() {
+		if (curSize != 18)
+			$('#content').css('font-size', 18);
+	});
+
+	$('#decreasetext').click(function() {
+		curSize = parseInt($('#content').css('font-size')) - 2;
+		if (curSize >= 14)
+			$('#content').css('font-size', curSize);
+	});
+// ENLARGE TEXT BOIIII END --------------------------------------------------------------------
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(window).scroll(function() {
+  var header = $(document).scrollTop();
+  var headerHeight = $(".header").outerHeight();
+  if (header > headerHeight) {
+    $(".header").addClass("fixed")
+      .css("background-color", "#232020");
+  } else {
+    $(".header").removeClass("fixed")
+    .css("background-color", "");
+  }
+});
+
+$(window).scroll(function() {
+  var header = $(document).scrollTop();
+  var headerHeight = $(".header").outerHeight();
+  var firstSection = $("#home_section").outerHeight() - 5;
+  if (header > headerHeight) {
+    $(".header").addClass("fixed");
+  } else {
+    $(".header").removeClass("fixed");
+  }
+  if (header > firstSection) {
+    $(".header").addClass("in-view");
+  } else {
+    $(".header").removeClass("in-view");
+  }
+});
+
+document.getElementById('media_control_btn_toggle').addEventListener('click', function() {    
+  if ( audio.muted ) {
+    document.getElementById('media_off').classList.add('media_hidden')
+    document.getElementById('media_on').classList.add('media_visible')
+    document.getElementById('media_on').classList.remove('media_hidden')
+    audio.muted = false;
+  } else {
+    audio.muted = true
+    document.getElementById('media_on').classList.add('media_hidden')
+    document.getElementById('media_off').classList.add('media_visible')
+    document.getElementById('media_off').classList.remove('media_hidden')
+  }
+  
+})
+
+let FONT_SIZE_LEVEL_MAX = 3
+let FONT_SIZE_LEVEL = 0
+const SELECTOR_TO_TOGGLE_TEXT_SIZE = [
+  '.slide_content_title',
+  '.text > p',
+]
+
+document.getElementById('increase_font').addEventListener('click', function() {
+  
+  if ( FONT_SIZE_LEVEL < FONT_SIZE_LEVEL_MAX ) { 
+    SELECTOR_TO_TOGGLE_TEXT_SIZE.forEach( function(cls) {
+      const originalFontSize = $(cls).css('font-size');
+      const originalFontNumber = parseFloat(originalFontSize, 10);
+      const newFontSize = originalFontNumber*1.1;
+      $(cls).css('font-size', newFontSize);
+    })
+    
+    
+    FONT_SIZE_LEVEL++
+  }
+
+})
+
+document.getElementById('decrease_font').addEventListener('click', function() {
+
+  if ( FONT_SIZE_LEVEL >= 0 ) { 
+    SELECTOR_TO_TOGGLE_TEXT_SIZE.forEach( function(cls) {
+      const originalFontSize = $(cls).css('font-size');
+      const originalFontNumber = parseFloat(originalFontSize, 10);
+      const newFontSize = originalFontNumber*0.8;
+      $(cls).css('font-size', newFontSize);
+    })
+    FONT_SIZE_LEVEL--
+  }
+
+})
---
+++ js/walkthrough.js
@@ -0,0 +1,174 @@
+  let FLOWERS_WALKTHROUGH = []
+  let FLOWERS_WALKTHROUGH_SHOWN = {}
+  const BASE_MAP_WIDTH = 660
+  const BASE_MAP_HEIGHT = 594
+  function convertResponseToJson(response) {  
+    return response.json()
+  }
+  function getParentWithFlowerId(element) {
+      let possibleFlowerIdEl = element     
+      while ( possibleFlowerIdEl ) {
+          const flowerId = possibleFlowerIdEl.getAttribute('data-id')
+          if ( Boolean(flowerId) ) {            
+              return possibleFlowerIdEl
+          }         
+          possibleFlowerIdEl = possibleFlowerIdEl.parentElement
+      }
+  }
+  function redrawLocations() {
+    const flowersToCleanup = Object.keys(FLOWERS_WALKTHROUGH_SHOWN).map( function(key) {
+      return FLOWERS_WALKTHROUGH_SHOWN[key]
+    })
+    flowersToCleanup.forEach(function(flower) {
+      removeCoordinates(flower.id)
+    })
+    flowersToCleanup.forEach(function(flower) {
+      drawLocationsOnMap(flower.id)
+    })
+  }
+
+  $(window).resize(function() {
+    redrawLocations()
+  })
+  function drawLocationsOnMap(id) {
+    
+    const flower = FLOWERS_MAP[id]
+    if (!FLOWERS_WALKTHROUGH_SHOWN[id] ) {
+      FLOWERS_WALKTHROUGH_SHOWN[id] = flower
+    }
+    // get current width height of image
+    const imageEl = document.getElementById('walkthough_img')
+    const currentWidth = imageEl.clientWidth
+    const currentHeight = imageEl.clientHeight
+
+    const resizedPositions = flower.locations.map(function(loc) {
+      return {        
+        left: loc.left/(BASE_MAP_WIDTH/currentWidth),
+        top: loc.top / ( BASE_MAP_HEIGHT/currentHeight)
+      }
+    })
+
+    const mapEl = document.getElementById('walkthrough_map')
+    
+    resizedPositions.forEach( location => { // left and top
+      const markerLocationEl = document.createElement('div')
+      markerLocationEl.classList.add(`${flower.id}-marker__`)
+      markerLocationEl.classList.add('marker_flower_anchor')
+      markerLocationEl.setAttribute('data-toggle','tooltip')      
+      markerLocationEl.setAttribute('title',flower.name)
+      markerLocationEl.style.position = 'absolute';
+
+      markerLocationEl.style.top = `${location.top}px`;
+      markerLocationEl.style.left = `${location.left}px`;
+      markerLocationEl.innerHTML = `
+          <img src='/img/marker.png' />
+      `
+      mapEl.appendChild(markerLocationEl)
+      markerLocationEl.addEventListener('click', () => {
+        $(markerLocationEl).tooltip('show')
+      })
+    })
+  }
+  function removeCoordinates(id) {
+      const markerClassName = `.${id}-marker__`
+      const mapEl = document.getElementById('walkthrough_map')
+      
+      const markerElements = document.querySelectorAll(markerClassName)
+
+      markerElements.forEach( function(markerEl) {
+          mapEl.removeChild(markerEl)
+      })
+      delete FLOWERS_WALKTHROUGH_SHOWN[id]
+  }
+
+
+  function handleFlowerClick(event) {
+    const targetEl = getParentWithFlowerId(event.target)
+    const id = targetEl.getAttribute('data-id')
+    if ( targetEl.classList.contains('walkthrough_filter_selected') ) {
+          // remove, being unselected
+          targetEl.classList.remove('walkthrough_filter_selected')        
+          // remove coordinates
+          removeCoordinates(id)
+    } else {
+      targetEl.classList.add('walkthrough_filter_selected')
+          // add coordinates
+      drawLocationsOnMap(id)
+    }
+  }
+
+  function renderFlowersDropdown() {
+      const windowWidth = document.body.clientWidth
+      let container = 'walkthrough_filter_anchor'
+      const filterToggle = document.getElementById('walkthrough_filter_toggle')
+      filterToggle.style.display = 'none'
+      if ( windowWidth < 900 ) {
+        container = 'walkthrough_filter'
+        filterToggle.style.display = 'block';
+      } 
+      
+      const flowersFilterEl = document.getElementById(container)
+      console.log('flowersFilterEl', flowersFilterEl, container)
+      FLOWERS_WALKTHROUGH.forEach(function(flower) {
+          if ( flower.locations && flower.locations.length > 0 ) {
+
+              const filterEl = document.createElement('div')              
+              filterEl.setAttribute('data-id', flower.id)              
+              filterEl.classList.add('walkthrough_flower')
+              const flowerName = flower.id.charAt(0).toUpperCase() + flower.id.substring(1)
+              // filterEl.innerHTML = flower.name
+              filterEl.innerHTML = `            
+                  <div class='walkthrough_filter_img'><img src="/img/marker.png"/></div>
+                  <div>${flowerName}</div>        
+              `
+              
+              filterEl.addEventListener('click', handleFlowerClick)
+
+              flowersFilterEl.appendChild(filterEl);
+          }    
+      })
+  }
+  
+  let popperInstance = undefined;
+  
+  document.getElementById('walkthrough_select_flowers').addEventListener('click', function() {    
+    if ( popperInstance ) {
+      return;
+    }
+    const reference = document.querySelector('#walkthrough_select_flowers');
+    const popper = document.querySelector('#walkthrough_filter');    
+    popperInstance = new Popper(reference, popper, {
+      placement: 'bottom'
+    });
+    $('#walkthrough_filter').css('visibility', 'visible')
+    let listener =  function(e) {
+      if ( popperInstance && e.target && !popper.contains(e.target) ) {
+        popperInstance.destroy();
+        popperInstance = undefined;
+        document.body.removeEventListener('click', listener)
+      }
+    }
+    setTimeout(function() {
+      document.body.addEventListener('click', listener) 
+    }, 0)
+    
+  })
+  function renderWalkthroughInterface(flowers) {    
+    FLOWERS_WALKTHROUGH = flowers;    
+    renderFlowersDropdown()
+  }
+
+  function getFlowers() {
+
+      fetch('/flowersdb.json')
+          .then( convertResponseToJson)
+          .then(renderWalkthroughInterface)
+  }
+
+
+  getFlowers()
+  
+  // $('.marker_flower_anchor').on("click",function(e){
+  //   alert('lol')
+  //   $(e).tooltip('show');
+  // });
